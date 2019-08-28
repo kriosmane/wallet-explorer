8f2eaf6640fead0c26c3056899a6bab266fae991
Krios Mane <krios.mane@gmail.com>
2019-08-28 11:23:46 +0200

    changed bitcoin explorer url

--- src/Cryptocurrencies/Bitcoin.php
+++ src/Cryptocurrencies/Bitcoin.php
@@ -19,42 +19,12 @@ class Bitcoin extends Crypto {
     /**
      * 
      */
-    protected $url = 'https://api-r.bitcoinchain.com/v1/address/%s';
+    protected $url = 'https://insight.bitpay.com/api/addr/%s/?noTxList=1';
 
     /**
      * 
      */
     protected $balance_response_path = 'balance';
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function handle($arguments)
-    {
-        
-        $response = $this->call($arguments);
-
-
-        if(!$response){
-
-            return false;
-
-        }
-
-        $response = json_decode($response->getBody()->getContents(), true);
-        
-        if(isset($response[0]['balance']))
-        {
-            $this->explorer_response->setBalance($response[0]['balance']);
-
-            return $this->explorer_response;
-        }
-
-        return false;
-
-    }
-
 
 
 }
